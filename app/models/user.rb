35cf8e7c5650c286b28e9873731e4ad76d425094
kfryauff <krista.fryauff@gmail.com>
2015-03-05 16:50:10 -0800

    placing static information on login screen to make room for home screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :notes
-  
+  has_many :students
+
   def recent_private_notes
     private_notes.limit(5)
   end
