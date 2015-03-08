705dc96aad32772f4bf645b89533d401fcc34fed
Evawere Ogbe <eoogbe@stanford.edu>
2015-03-08 14:48:27 -0700

    Modded navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
   def url_with_query url, query
     url + (url.include?('?') ? '&' : '?') + query
   end
+  
+  def navbar_menuitems
+    [["Home", students_path],
+      ["Create new note", new_note_students_path],
+      ["Add students to your home", "#"],
+      ["Remove students from your home", "#"],
+      ["Create a new student profile", new_student_path],
+      ["Delete a student profile", "#"]]
+  end
 end
